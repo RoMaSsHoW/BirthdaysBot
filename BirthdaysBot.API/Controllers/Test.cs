3267e68dd76b570c54e5eb4408c7d0063e4a5425
Roman Saharov <fantomfantomovich0@gmail.com>
2024-12-24 17:57:55 +0500

    Добавьте файлы проекта.

---
+++ BirthdaysBot.API/Controllers/Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdaysBot.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Test : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> TestData()
+        {
+            var data = await GetTestData();
+
+            return data == null ? NotFound() : Ok(data);
+        }
+
+        private async Task<IEnumerable<int>> GetTestData()
+        {
+            List<int> data = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            return data;
+        }
+    }
+}
