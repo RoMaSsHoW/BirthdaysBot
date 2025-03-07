2ed494d4dc8aefded6500dec89debec4c88390db
Roman Saharov <fantomfantomovich0@gmail.com>
2025-03-07 20:49:44 +0500

    длбавил контекст

---
+++ BirthdaysBot.DAL/Entities/Birthday.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BirthdaysBot.DAL.Entities;
+
+public partial class Birthday
+{
+    public int BirthdayId { get; set; }
+
+    public string? BirthdayName { get; set; }
+
+    public DateOnly? BirthdayDate { get; set; }
+
+    public string? BirthdayTelegramUsername { get; set; }
+
+    public long? UserChatId { get; set; }
+}
